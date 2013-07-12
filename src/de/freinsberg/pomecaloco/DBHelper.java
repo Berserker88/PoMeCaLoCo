8256e7e16227bc5b64eaea55abbcf7521f8fc0ad
Berserker88 <florian.reinsberg@googlemail.com>
2013-07-12 14:53:26 +0200

    rewritten my database schema and implemented some new methods

--- src/de/freinsberg/pomecaloco/DBHelper.java
+++ src/de/freinsberg/pomecaloco/DBHelper.java
@@ -1,6 +1,11 @@
 package de.freinsberg.pomecaloco;
 
+import java.util.ArrayList;
+
+import org.opencv.core.Mat;
+
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import android.database.sqlite.SQLiteOpenHelper;
@@ -10,6 +15,9 @@ public class DBHelper extends SQLiteOpenHelper{
 	
 	private static final String DB_NAME = "pomecaloco.db";
 	private static final int DB_VERSION = 1;
+	
+	private byte[] bridge_image;
+	private byte[] crossed_image;
 
 	private SQLiteDatabase mDB;
 	
@@ -19,11 +27,12 @@ public class DBHelper extends SQLiteOpenHelper{
 	}
 	//following section declares the database layout
 	
-	private class TblTracks {
+	private class TblTrack {
 		
 		private static final String NAME = "tracks";
 		private static final String COL_NAME = "name";
 		private static final String COL_LENGTH = "length";
+		
 		private static final String COL_ISCROSSED = "iscrossed";
 		private static final String COL_IMAGE = "image";		
 	}
@@ -33,6 +42,7 @@ public class DBHelper extends SQLiteOpenHelper{
 		private static final String NAME = "player_track";
 		private static final String COL_TRACKNAME = "track_name";
 		private static final String COL_PLAYERNAME = "player_name";
+		private static final String COL_MODE = "mode";
 		private static final String COL_ATTEMPT = "attempt";
 		private static final String COL_FASTESTROUND = "fastestround";
 		private static final String COL_LASTDRIVENMETERS = "lastdrivenmeters";
@@ -44,19 +54,24 @@ public class DBHelper extends SQLiteOpenHelper{
 		
 		private static final String NAME = "player";
 		private static final String COL_NAME = "name";
-		private static final String COL_MODE = "mode";
-		private static final String COL_LANE = "lane";
 		private static final String COL_COLOR = "color";
-		private static final String COL_INTCOLOR = "intcolor";	
+		private static final String COL_INTCOLOR = "intcolor";
+		private static final String COL_WHOLEDRIVENMETERS ="wholedrivenmeters";
 	}
 	
-	private class TblGhost {
+	private class TblRoundGhost {
 		
-		private static final String NAME = "ghost";
+		private static final String NAME = "roundghost";
+		private static final String COL_NAME = "track_name";		
+		private static final String COL_ROUNDS = "rounds";		
+		private static final String COL_TIMES = "times";
+	}
+	
+	private class TblTimeGhost {
+		
+		private static final String NAME = "timeghost";
 		private static final String COL_NAME = "track_name";
-		private static final String COL_MODE = "mode";
-		private static final String COL_LANE = "lane";
-		private static final String COL_ROUNDS = "rounds";
+		private static final String COL_TIME = "time";
 		private static final String COL_TIMES = "times";
 	}
 
@@ -66,55 +81,230 @@ public class DBHelper extends SQLiteOpenHelper{
 		
 		//Creating the Tracks Table
 		db.execSQL(
-					"CREATE TABLE" + TblTracks.NAME + 
-					"("+ TblTracks.COL_NAME + " VARCHAR(30) PRIMARY KEY,"
-					+ TblTracks.COL_LENGTH + " INTEGER,"
-					+ TblTracks.COL_ISCROSSED + " BOOLEAN,"
-					+ TblTracks.COL_IMAGE + " BLOB)"				
-				);
+				"CREATE TABLE" + TblTrack.NAME + 
+				"("+ TblTrack.COL_NAME + " VARCHAR(30) PRIMARY KEY,"
+				+ TblTrack.COL_LENGTH + " INTEGER,"				
+				+ TblTrack.COL_ISCROSSED + " BOOLEAN,"				
+				+ TblTrack.COL_IMAGE + " BLOB)"				
+			);
 		
 		//Creating the Player Table
 		db.execSQL(
-					"CREATE TABLE" + TblPlayer.NAME + 
-					"("+ TblPlayer.COL_NAME + " VARCHAR(30) PRIMARY KEY,"
-					+ TblPlayer.COL_MODE + " INTEGER,"
-					+TblPlayer.COL_LANE + " INTEGER,"
-					+TblPlayer.COL_COLOR + " VARCHAR(16),"
-					+TblPlayer.COL_INTCOLOR + " INTEGER)"			
-				);
+				"CREATE TABLE" + TblPlayer.NAME + 
+				"("+ TblPlayer.COL_NAME + " VARCHAR(30) PRIMARY KEY,"				
+				+TblPlayer.COL_COLOR + " VARCHAR(16),"
+				+TblPlayer.COL_INTCOLOR + " INTEGER, "
+				+ TblPlayer.COL_WHOLEDRIVENMETERS + " FLOAT(5))"
+			);
 		
-		//Creating the Ghost Table
+		//Creating the RoundGhost Table
 		db.execSQL(
-				"CREATE TABLE" + TblGhost.NAME + 
-				"(" + TblGhost.COL_NAME + " VARCHAR(30) PRIMARY KEY REFERENCES " + TblTracks.NAME + "(" + TblTracks.COL_NAME + "), "
-				+ TblGhost.COL_MODE + " INTEGER,"
-				+TblGhost.COL_LANE + " INTEGER,"
-				+TblGhost.COL_ROUNDS + " INTEGER,"
-				+TblGhost.COL_TIMES + " VARCHAR(10000))"
-				);
+				"CREATE TABLE" + TblRoundGhost.NAME + 
+				"(" + TblRoundGhost.COL_NAME + " VARCHAR(30) PRIMARY KEY REFERENCES " + TblTrack.NAME + "(" + TblTrack.COL_NAME + "), "							
+				+TblRoundGhost.COL_ROUNDS + " INTEGER PRIMARY KEY,"				
+				+TblRoundGhost.COL_TIMES + " VARCHAR(10000))"
+			);
+		
+		//Creating the TimeGhost Table
+		db.execSQL(
+				"CREATE TABLE" + TblTimeGhost.NAME + 
+				"(" + TblTimeGhost.COL_NAME + " VARCHAR(30) PRIMARY KEY REFERENCES " + TblTrack.NAME + "(" + TblTrack.COL_NAME + "), "	
+				+TblTimeGhost.COL_TIME + " INTEGER PRIMARY KEY,"
+				+TblTimeGhost.COL_TIMES + " VARCHAR(10000))"
+			);
+		
 		
 		//Creating the Player-Track Table
 		db.execSQL(
 				"CREATE TABLE" + TblPlayer_Track.NAME +
-				"(" + TblPlayer_Track.COL_PLAYERNAME + " VARCHAR(30) PRIMARY KEY REFERENCES " +TblTracks.NAME + "(" + TblTracks.COL_NAME + "), "
+				"(" + TblPlayer_Track.COL_PLAYERNAME + " VARCHAR(30) PRIMARY KEY REFERENCES " +TblTrack.NAME + "(" + TblTrack.COL_NAME + "), "
 				+ TblPlayer_Track.COL_TRACKNAME + "VARCHAR(30) PRIMARY KEY REFERENCES " + TblPlayer.NAME + "(" + TblPlayer.COL_NAME + "), "
+				+ TblPlayer_Track.COL_MODE + "INTEGER PRIMARY KEY, "
 				+ TblPlayer_Track.COL_ATTEMPT + " INTEGER,"
 				+ TblPlayer_Track.COL_FASTESTROUND + " VARCHAR(9),"
-				+ TblPlayer_Track.COL_LASTDRIVENMETERS + " FLOAT(5),"
-				+ TblPlayer_Track.COL_WHOLEDRIVENMETERS + " FLOAT(5),"
+				+ TblPlayer_Track.COL_LASTDRIVENMETERS + " FLOAT(5),"				
 				+ TblPlayer_Track.COL_LASTAVERAGESPEED + " FLOAT(5),"
 				+ TblPlayer_Track.COL_WHOLEAVERAGESPEED + " FLOAT(5),"
-				);		
+			);		
+			createTrack("Brückenbahn", false, (float)3.75, bridge_image);
+			createTrack("Kreuzungsbahn", true, (float)5.50, crossed_image);					
 	}
 
 	public void openDB(){
 		
 		mDB = getWritableDatabase();
 	}
-	private void initDatabase(){
-		
+	
+	private void initDatabase(){		
 		
 	}
+	
+	public void createPlayer(String name, String color, int int_color, int wholedrivenmeters){
+		mDB.execSQL(
+				"INSERT INTO "+ TblPlayer.NAME + "(" 
+				+ TblPlayer.COL_NAME + ", " 				
+				+ TblPlayer.COL_COLOR + ", " 
+				+ TblPlayer.COL_INTCOLOR + ", "
+				+ TblPlayer.COL_WHOLEDRIVENMETERS + ")"
+				+ "VALUES('" + name + "', " + color + "', " + int_color + ", " + wholedrivenmeters +")"
+			);
+	}
+		
+	private void createTrack(String name, boolean iscrossed, float length, byte[] image){
+		mDB.execSQL(
+				"INSERT INTO " + TblTrack.NAME + "(" 
+				+ TblTrack.COL_NAME + ", " 
+				+ TblTrack.COL_ISCROSSED + ", " 
+				+ TblTrack.COL_LENGTH + ", " 
+				+ TblTrack.COL_IMAGE + ")"
+				+ "VALUES('" + name + "', " + iscrossed + ", '" + length + "', " + image + ")"
+			);
+	}
+	
+	public void createRoundGhost(String name, int rounds, String times) {
+		
+		mDB.execSQL(
+				"INSERT INTO " + TblRoundGhost.NAME + "("
+				+ TblRoundGhost.COL_NAME + ", " + TblRoundGhost.COL_ROUNDS + ", " +  TblRoundGhost.COL_TIMES + ")"
+				+ "VALUES('" + name + "', " + rounds + ", '" + times + "')"				
+			);
+	}
+	
+	public void createTimeGhost(String name, int time, String times) {
+		
+		mDB.execSQL(
+				"INSERT INTO " + TblTimeGhost.NAME + "("
+				+ TblTimeGhost.COL_NAME + ", " + TblTimeGhost.COL_TIME + ", " + TblTimeGhost.COL_TIMES + ")"
+				+ "VALUES('" + name + "', " + time + "', '" + times + "')"				
+			);
+	}
+	
+	public void createPlayerTrack(String playername, String trackname, int mode, int attempt, String fastestround, float lastaveragespeed, float wholeaveragespeed, float lastdrivenmeters){
+		
+		mDB.execSQL(
+				"INSERT INTO "+ TblPlayer_Track.NAME + "("
+				+ TblPlayer_Track.COL_PLAYERNAME + ", "
+				+ TblPlayer_Track.COL_TRACKNAME + ", "
+				+ TblPlayer_Track.COL_MODE + ", "
+				+ TblPlayer_Track.COL_ATTEMPT + ", "
+				+ TblPlayer_Track.COL_FASTESTROUND + ", "
+				+ TblPlayer_Track.COL_LASTAVERAGESPEED + ", "
+				+ TblPlayer_Track.COL_WHOLEAVERAGESPEED + ", "
+				+ TblPlayer_Track.COL_LASTDRIVENMETERS + ", "				
+				+ "VALUES('" + playername + "', '" + trackname + "', " + mode + ", " + attempt + ", '" + fastestround + "', '" + lastaveragespeed + "','" + wholeaveragespeed + "','" + lastdrivenmeters + ")"
+			);
+	}
+	
+	public ArrayList<Pair<String,byte[]>> getAllTracksWithImages(){
+		ArrayList<Pair<String,byte[]>> tracks = new ArrayList<Pair<String,byte[]>>();
+		Cursor cursor = mDB.query(TblTrack.NAME, new String[]{TblTrack.COL_NAME,TblTrack.COL_IMAGE}, null, null, null, null, TblTrack.COL_NAME);
+		
+		cursor.moveToFirst();
+		
+		while(!cursor.isAfterLast()){
+			
+			tracks.add(new Pair<String, byte[]>(cursor.getString(0), cursor.getBlob(1)));
+			cursor.moveToNext();			
+		}
+		
+		cursor.close();	
+		
+		return tracks;	
+		
+	}
+	
+	public float getTrackLength(String name){
+		float length;
+		
+		Cursor cursor = mDB.query(TblTrack.NAME, new String[]{TblTrack.COL_LENGTH}, TblTrack.COL_NAME, new String[]{name}, null, null, null);
+		
+		cursor.moveToFirst();
+		
+		length = cursor.getFloat(0);
+		
+		return length;
+		
+	}
+	
+	public ArrayList<String> getAllPlayernames(){
+		ArrayList<String> names = new ArrayList<String>();
+		Cursor cursor = mDB.query(TblPlayer.NAME, new String[]{TblPlayer.COL_NAME}, null, null, null, null, TblPlayer.COL_NAME);
+		
+		cursor.moveToFirst();
+		
+		while(!cursor.isAfterLast()){
+			
+			names.add(cursor.getString(0));
+			cursor.moveToNext();			
+		}
+		return names;
+	}
+	
+	public String getPlayerColor(String name){
+		String color;
+		
+		Cursor cursor = mDB.query(TblPlayer.NAME, new String[]{TblPlayer.COL_COLOR}, TblPlayer.COL_NAME, new String[]{name}, null, null, null);
+		
+		cursor.moveToFirst();
+		
+		color = cursor.getString(0);
+		
+		return color;		
+	}
+	
+	public int getPlayerIntColor(String name){
+		int intcolor;
+		
+		Cursor cursor = mDB.query(TblPlayer.NAME, new String[]{TblPlayer.COL_INTCOLOR}, TblPlayer.COL_NAME, new String[]{name}, null, null, null);
+		
+		cursor.moveToFirst();
+		
+		intcolor = cursor.getInt(0);
+		
+		return intcolor;		
+	}
+	
+	public float getPlayerWholeDrivenMeters(String name) {
+		float meters;
+		
+		Cursor cursor = mDB.query(TblPlayer.NAME, new String[]{TblPlayer.COL_WHOLEDRIVENMETERS}, TblPlayer.COL_NAME, new String[]{name}, null, null, null);
+		
+		cursor.moveToFirst();
+		
+		meters = cursor.getFloat(0);
+		
+		return meters;	
+		
+	}
+	
+	public String getRoundGhost(String track, int rounds) {
+		String times;
+		
+		String whereClause = TblRoundGhost.COL_ROUNDS+"='?' AND "+ TblRoundGhost.COL_NAME +"='?'";
+		
+		Cursor cursor = mDB.query(TblRoundGhost.NAME, new String[]{TblRoundGhost.COL_TIMES}, whereClause, new String[]{""+rounds, track}, null, null, null);		
+		cursor.moveToFirst();
+		
+		times = cursor.getString(0);
+		
+		return times;		
+	}
+	
+	public String getTimeGhost(String track, int time) {
+		String times;
+		
+		String whereClause = TblTimeGhost.COL_TIME+"='?' AND "+ TblTimeGhost.COL_NAME +"='?'";
+		
+		Cursor cursor = mDB.query(TblTimeGhost.NAME, new String[]{TblTimeGhost.COL_TIMES}, whereClause, new String[]{""+time, track}, null, null, null);		
+		cursor.moveToFirst();
+		
+		times = cursor.getString(0);
+		
+		return times;		
+	}
+
+	
+	
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 		// TODO Automatisch generierter Methodenstub
